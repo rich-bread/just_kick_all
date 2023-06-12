924619d80a5502f226523337993bae1cd060e626
RichBread <royal.richbread@gmail.com>
2023-06-12 12:34:41 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+import platform
+import discord
+from discord.ext import commands
+from colorama import Back, Fore, Style
+from datetime import datetime
+
+class Client(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned, intents=discord.Intents.all()) #you must enable ALL intents
+        self.cogslist = ['kick_all'] #add cog files name in here
+
+    async def setup_hook(self):
+        if self.cogslist:
+            for ext in self.cogslist:
+                await self.load_extension(ext)
+
+    async def on_ready(self):
+        prfx = (Fore.RESET + Style.RESET_ALL + Back.BLACK + Fore.GREEN + datetime.now().strftime(r'%Y-%m-%d %H:%M:%S') + Back.RESET + Fore.WHITE + Style.BRIGHT)
+        print(prfx + " Logged in as " + Fore.YELLOW + self.user.name)
+        print(prfx + " Bot ID " + Fore.YELLOW + str(self.user.id))
+        print(prfx + " Discord Version " + Fore.YELLOW + discord.__version__)
+        print(prfx + " Python Version " + Fore.YELLOW + str(platform.python_version()))
+        synced = await self.tree.sync()
+        print(prfx + " Slash Commands Synced " + Fore.YELLOW + str(len(synced)) + " Commands")
+        
+
+client = Client()
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+client.run(TOKEN)
---
+++ kick_all.py
@@ -0,0 +1,46 @@
+import json
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+cmd_file = open('cmd_kick.json') #open command file
+cmd_data = json.load(cmd_file) #load command file
+
+class KickAll(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.cmd_data = cmd_data
+
+    @app_commands.command(name=cmd_data['name'], description=cmd_data['description'])
+    @app_commands.checks.has_role(cmd_data['checks']['role_id'])
+    async def kick_all(self, interaction:discord.Interaction):
+        try:
+            await interaction.response.defer(ephemeral=True, thinking=True) #make interaction defer
+            members = interaction.guild.members #get all members from guild
+            avoids_file = open('avoids.json') #open avoid list
+            avoids_data = json.load(avoids_file) #load avoid list
+            members_id = avoids_data['members_id'] #get members id who you want to avoid from kicking
+            roles_id = avoids_data['roles_id'] #get roles id which you want to avoid from kicking
+            for member in members:
+                if member.id in members_id: continue #continue if the member is in the avoid list
+                for member_role in member.roles:
+                    if member_role.id in roles_id: continue #continue if the member has the role to be avoided
+                await member.kick() #kick member / if you want to add a reason, add here
+
+        except Exception as e:
+            emsg = self.cmd_data['messages']['error']['on_exception'].format(content=e) #error message
+            await interaction.followup.send(content=emsg, ephemeral=True) #send error message as ephemeral
+
+        else:
+            smsg = self.cmd_data['messages']['success']['on_success'] #success message
+            await interaction.followup.send(content=smsg, ephemeral=True) #send success message as ephemeral
+
+    @kick_all.error
+    async def on_kick_all_error(self, interaction:discord.Interaction, error:app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingRole): #error when command author was missing required role
+            smsg = self.cmd_data['messages']['error']['on_missing_role'] #error message
+            await interaction.response.send_message(content=smsg, ephemeral=True) #send success message as ephemeral
+
+
+async def setup(client: commands.Bot):
+    await client.add_cog(KickAll(client)) #add cog to client
